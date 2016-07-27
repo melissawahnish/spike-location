9fc51d10aed32aa83d525f0666134f3fda82fcec
Melissa Wahnish <melissawahnish@gmail.com>
2016-07-27 12:10:16 -0400

    Refactor to better describe validations and make more positive methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,27 +4,26 @@ class Reservation < ActiveRecord::Base
 	validate :dates_are_available
 
 	def dates_are_available
-	  if !confirm_available_dates || !confirm_no_reservation_overlap
+	  if reservation_overlap? || dates_unavailable?
 	    errors[:base] << "Some of the dates of your reservation are not 
 	    available.  Please try different dates."
 	  end
 	end
 
-	def confirm_no_reservation_overlap
-
+	def reservation_overlap?
 	  start_date_overlap = location.reservations.where(start_date: 
 	    start_date..end_date - 1.day)
 	  end_date_overlap = location.reservations.where(end_date:  
 	    start_date..end_date - 1.day)
-	  !start_date_overlap.exists? || !end_date_overlap.exists?
+	  start_date_overlap.exists? || end_date_overlap.exists?
 	end
 
 
-	def confirm_available_dates
+	def dates_unavailable?
 		reservation_array =  (start_date..end_date).to_a
 		available_dates = AvailableDate.where(location_id: location.id).where(available_date: reservation_array)
 		non_reserved_available_dates = available_dates.where(reserved: false)
-		non_reserved_available_dates.count == reservation_array.count
+		non_reserved_available_dates.count != reservation_array.count
 	end
 
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,27 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  describe "#confirm_available_dates" do
-  	it "returns true if there are available_dates for the reservation" do
+  describe "#no_available_dates?" do
+  	it "returns true if there are not available_dates for the reservation" do
+  		location = FactoryGirl.create(:location)
+  		reservation = Reservation.new(
+  			start_date: Date.tomorrow, 
+  			end_date: Date.today + 2.days,
+  			location_id: location.id )
+
+  		expect(reservation.dates_unavailable?).to eq true
+  	end
+  	it "returns false if there are available_dates for the reservation" do
   		location = FactoryGirl.create(:location_with_available_dates)
   		reservation = Reservation.new(
   			start_date: Date.tomorrow, 
   			end_date: Date.today + 2.days,
   			location_id: location.id )
 
-  		expect(reservation.confirm_available_dates).to eq true
+  		expect(reservation.dates_unavailable?).to eq false
   	end
   end
 
-  describe "#confirm_no_reservation_overlap" do
-  	it "returns true if there are available_dates for the reservation" do
+  describe "#reservation_overlap?" do
+  	it "returns true if there are reservations during that date range" do
+  		location = FactoryGirl.create(:location_with_available_dates)
+  		existing_reservation = Reservation.create(
+  			start_date: Date.tomorrow, 
+  			end_date: Date.today + 2.days,
+  			location_id: location.id )
+  		reservation = Reservation.new(
+  			start_date: Date.tomorrow, 
+  			end_date: Date.today + 2.days,
+  			location_id: location.id )
+
+  		expect(reservation.reservation_overlap?).to eq true
+  	end
+  	it "returns false if there are not reservations during that date range" do
   		location = FactoryGirl.create(:location_with_available_dates)
   		reservation = Reservation.new(
   			start_date: Date.tomorrow, 
   			end_date: Date.today + 2.days,
   			location_id: location.id )
 
-  		expect(reservation.confirm_no_reservation_overlap).to eq true
+  		expect(reservation.reservation_overlap?).to eq false
   	end
   end
 
